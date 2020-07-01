d9d9e9d72aa7dc6ab1a82005778d61dce29b9b71
serginho6x <serginho_6x@hotmail.com>
2020-07-01 17:55:50 -0400

    refactory

--- serial_communication5.ino
+++ serial_communication5.ino
@@ -1,24 +1,21 @@
-/*
-  AnalogReadSerial
+#include <ArduinoJson.h>
+#define BAUDRATE 19200
+#define TWOMILLISECONDS 2
+int sensorValue;
 
-  Reads an analog input on pin 0, prints the result to the Serial Monitor.
-  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogReadSerial
-*/
-
-void setup() {
-  
-  Serial.begin(9600);
+void sendPotentiometerData()
+{
+  sensorValue= analogRead(A0); 
+  Serial.println(sensorValue);      
 }
 
-void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  delay(2);        // delay in between reads for stability
+void setup()
+{
+  Serial.begin(BAUDRATE);
+}
+
+void loop()
+{
+  sendPotentiometerData();
+  delay(TWOMILLISECONDS); 
 }
